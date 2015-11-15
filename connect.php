623509b9762ac6d7f8fe31bfa433883ab89f2200
alhaura19 <haura19@gmail.com>
2015-11-15 14:17:41 +0700

    Ubah hello.php

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?
+$hostname = "br-cdbr-azure-south-a.cloudapp.net";
+$username = "b5d3a2fe926939";
+$password = "1ac57d24";
+$database_name = "db_webapp";
+
+$conn = mysql_connect($hostname, $username, $password)
+	or die ("Error menghubungkan ke host database");
+$db = mysql_select_db($database_name)
+	or die ("Error menghubungkan ke database");
+?>
