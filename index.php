3b42783fda8a152c7a87fb93429c44d1b4fd3729
Alhaura Latifah <Alhaura Latifah>
2015-11-15 13:03:25 +0700

    initial commit: Hello, Azure!

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-	echo"Hai!";
-?>
+<html>
+<head>
+	<title>Web App Sederhana</title>
+</head>
+<body>
+	<form action="hello.php" method="POST">
+		Nama : <input type="text" name="nama"/>
+		<br>
+		Email : <input type="text" name="email"/>
+		<br>
+		<input type="submit" name="Enter"/>
+	</form>
+</body>
+</html>
